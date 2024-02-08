5ff5db53e33aacd21a6c9031613bec6cc871a85b
Lukas Stuffer <159128382+LukasStuffer@users.noreply.github.com>
2024-02-08 09:32:09 +0100

    main.py

--- main.py
+++ main.py
@@ -10,42 +10,44 @@ import subprocess
 from datetime import datetime
 from colorama import Fore, Style
 
-print ("\n2023-2024 (c) Lukas Stuffer")
-print ("IG: _lukasstuffer_")
-
-
-# name/description : command
-work = [
-    ['DB-Maintenance'   , 'python3 /var/www/python/maintenance/main.py'],
-    ['Virustotal-Check' , 'python3 /var/www/python/virustotal/main.py']
-]
-
-
-# start work
-for i in range(len(work)):
-
-    # start script
-    print (f"\n> {Fore.GREEN}{Style.BRIGHT}START{Style.RESET_ALL} {work[i][0]}")
-    cmd = str(work[i][1])
-
-    try:
-
-        result = subprocess.run(cmd, shell=True, check=True, text=True)
-
-    except Exception as e:
-        print(f"> {Fore.RED}{Style.BRIGHT}ERROR{Style.RESET_ALL} {e}")
+if __name__ == "__main__":
     
-
-# debug/heartbeat
-try:
-    time_now = str(datetime.now()).split('.')[0]
-
-    file_path = './last_call.txt'
-    with open(file_path, 'w') as output:
-        output.write(time_now)
-
-except Exception as e:
-    print (f"\n> {Fore.RED}{Style.BRIGHT}ERROR{Style.RESET_ALL} {e}")
-
-
-print(f"\n> END")
+    print ("\n2023-2024 (c) Lukas Stuffer")
+    print ("IG: _lukasstuffer_")
+    
+    
+    # name/description : command
+    work = [
+        ['DB-Maintenance'   , 'python3 /var/www/python/maintenance/main.py'],
+        ['Virustotal-Check' , 'python3 /var/www/python/virustotal/main.py']
+    ]
+    
+    
+    # start work
+    for i in range(len(work)):
+    
+        # start script
+        print (f"\n> {Fore.GREEN}{Style.BRIGHT}START{Style.RESET_ALL} {work[i][0]}")
+        cmd = str(work[i][1])
+    
+        try:
+    
+            result = subprocess.run(cmd, shell=True, check=True, text=True)
+    
+        except Exception as e:
+            print(f"> {Fore.RED}{Style.BRIGHT}ERROR{Style.RESET_ALL} {e}")
+        
+    
+    # debug/heartbeat
+    try:
+        time_now = str(datetime.now()).split('.')[0]
+    
+        file_path = './last_call.txt'
+        with open(file_path, 'w') as output:
+            output.write(time_now)
+    
+    except Exception as e:
+        print (f"\n> {Fore.RED}{Style.BRIGHT}ERROR{Style.RESET_ALL} {e}")
+    
+    
+    print(f"\n> END")
